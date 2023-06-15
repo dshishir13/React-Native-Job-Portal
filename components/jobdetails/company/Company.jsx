4d456d373a7bbdf08d08e8de76d80b70a2420606
Shishir Lamichhane <103579434+dshishir13@users.noreply.github.com>
2023-06-15 04:02:41 +0000

    working commit

---
+++ components/jobdetails/company/Company.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+
+import styles from "./company.style";
+import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
+
+const Company = ({ companyLogo, jobTitle, companyName, Location }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa6ws_wKLsz9mEx1azqjZtoa0acEtIdGbhyqaykkOB&s",
+          }}
+          style={styles.logoImage}
+        />
+      </View>
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} / </Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode="contain"
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{Location}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Company;
